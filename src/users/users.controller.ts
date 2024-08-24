0686a62ae45d1f4aa3887ea1201c47bbbb876416
Jordach Makaya H.M <4thinkai@gmail.com>
2024-08-24 20:24:33 +0100

    createuser logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,35 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+} from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { UsersService } from "./users.service";
+import { DuplicateEmailException } from "./CustomExceptions/duplicate-email.exception";
 
 @Controller("auth")
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Post("/signup")
-  createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+  async createUser(@Body() body: CreateUserDto) {
+    try {
+      await this.usersService.create(body.email, body.password);
+      console.log(
+        `User with email ${body.email} was successfully added to the database`
+      );
+    } catch (err) {
+      // Handle specific errors or use NestJS exceptions here
+      // For example:
+      if (err instanceof DuplicateEmailException) {
+        throw new BadRequestException("Email already exists");
+      } else {
+        throw new InternalServerErrorException(
+          "`Failed to persist User with email ${body.email} because of ${err.message}`"
+        );
+      }
+    }
   }
 }
